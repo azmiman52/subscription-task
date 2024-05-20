045bc6afd90b67da65babce9ef2ea3851aa00805
azmiman52 <azmi.iman52@gmail.com>
2024-05-21 01:21:18 +0700

    feat: complete task 2 and some task from other

---
+++ src/test/java/id/ac/ui/cs/advprog/subscription/domains/SubscriptionTest.java
@@ -0,0 +1,53 @@
+package id.ac.ui.cs.advprog.subscription.domains;
+
+import id.ac.ui.cs.advprog.subscription.domains.entities.Subscription;
+import id.ac.ui.cs.advprog.subscription.domains.entities.SubscriptionBox;
+import id.ac.ui.cs.advprog.subscription.domains.enums.SubscriptionStatus;
+import id.ac.ui.cs.advprog.subscription.domains.enums.SubscriptionType;
+import id.ac.ui.cs.advprog.subscription.mock.Box;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SubscriptionTest {
+
+    private Subscription mockSubscription;
+    private String subscriptionId;
+    private SubscriptionBox subscriptionBox;
+
+    @BeforeEach
+    void setUp() {
+        subscriptionBox = new SubscriptionBox();
+        mockSubscription = new Subscription("subscription-1", SubscriptionType.MONTHLY, SubscriptionStatus.PENDING, subscriptionBox);
+    }
+
+    @Test
+    void testCreateSubscription() {
+        Subscription subscription = new Subscription("subscription-1", SubscriptionType.MONTHLY, SubscriptionStatus.PENDING, subscriptionBox);
+
+        assertEquals("subscription-1", subscription.getId());
+        assertEquals(SubscriptionType.MONTHLY, subscription.getType());
+        assertEquals(SubscriptionStatus.PENDING, subscription.getStatus());
+        assertEquals(subscriptionBox, subscription.getSubscriptionBox());
+    }
+
+    @Test
+    void testCreateInvalidSubscription() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Subscription(null, SubscriptionType.MONTHLY, SubscriptionStatus.PENDING, subscriptionBox);
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Subscription("subscription-1", null, SubscriptionStatus.PENDING, subscriptionBox);
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Subscription("subscription-1", SubscriptionType.MONTHLY, null, subscriptionBox);
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Subscription("subscription-1", SubscriptionType.MONTHLY, SubscriptionStatus.PENDING, null);
+        });
+    }
+}
